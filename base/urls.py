d4fb87991264921a2e9884e0ae8d58376837c91e
Ruslan Muzyra <rmuzyra20@gmail.com>
2023-02-10 23:59:47 +0200

    fixed bug

--- base/urls.py
+++ base/urls.py
@@ -3,7 +3,7 @@ from base.views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete,
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('login/', CustomLoginView.as_view(), name='login'),
+    path('login/', CustomLoginView.as_view(), name='login')
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
